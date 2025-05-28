fb73ca4c178ad4a5482b1571c94768a1644fc29f
Jake Reck <jakereck@gmail.com>
2025-05-28 09:39:42 -0500

    Adding file storage

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,9 +1,11 @@
 using System.Linq;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using AvaloniaTodo.Services;
 using AvaloniaTodo.ViewModels;
 using AvaloniaTodo.Views;
 
@@ -16,20 +18,54 @@ namespace AvaloniaTodo
             AvaloniaXamlLoader.Load(this);
         }
 
-        public override void OnFrameworkInitializationCompleted()
+        private readonly MainWindowViewModel _mainWindowViewModel = new MainWindowViewModel();
+
+        public override async void OnFrameworkInitializationCompleted()
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-                // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
                 DisableAvaloniaDataAnnotationValidation();
                 desktop.MainWindow = new MainWindow
                 {
-                    DataContext = new MainWindowViewModel(),
+                    DataContext = _mainWindowViewModel,
                 };
-            }
 
+                desktop.ShutdownRequested += DesktopOnShutdownRequested;
+            }
+            await InitMainViewModelAsync();
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private bool _canClose;
+        private async void DesktopOnShutdownRequested(object? sender, ShutdownRequestedEventArgs e)
+        {
+            e.Cancel = !_canClose;
+
+            if (!_canClose)
+            {
+                var itemsToSave = _mainWindowViewModel.ToDoItems.Select(item => item.GetToDoItem());
+                await ToDoListFileService.SaveFileToFileAsync(itemsToSave);
+
+                _canClose = true;
+
+                if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                {
+                    desktop.Shutdown();
+                }
+            }
+        }
+
+        private async Task InitMainViewModelAsync()
+        {
+            var itemsLoaded = await ToDoListFileService.LoadFromFileAsync();
+
+            if (itemsLoaded != null)
+            {
+                foreach (var item in itemsLoaded)
+                {
+                    _mainWindowViewModel.ToDoItems.Add(new ToDoItemViewModel(item));
+                }
+            }
         }
 
         private void DisableAvaloniaDataAnnotationValidation()
---
+++ Services/ToDoListFileService.cs
@@ -0,0 +1,39 @@
+﻿using AvaloniaTodo.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AvaloniaTodo.Services;
+
+public static class ToDoListFileService
+{
+    private static string _jsonFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TodoList", "MyToDoList.txt");
+
+    public static async Task SaveFileToFileAsync(IEnumerable<ToDoItem> itemsToSave)
+    {
+        Directory.CreateDirectory(Path.GetDirectoryName(_jsonFileName)!);
+
+        using (var fs = File.Create(_jsonFileName))
+        {
+            await JsonSerializer.SerializeAsync(fs, itemsToSave);
+        }
+
+    }
+
+    public static async Task<IEnumerable<ToDoItem>?> LoadFromFileAsync()
+    {
+        try
+        {
+            using (var fs = File.OpenRead(_jsonFileName))
+            {
+                return await JsonSerializer.DeserializeAsync<IEnumerable<ToDoItem>>(fs);
+            }
+        }
+        catch (Exception e) when (e is FileNotFoundException || e is DirectoryNotFoundException)
+        {
+            return null;
+        }
+    }
+}
